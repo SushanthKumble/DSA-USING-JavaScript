dfe09e75c317d12cf4fa07944b4828564fe09fff
SUSHANTH K <127724278+SushanthKumble@users.noreply.github.com>
2023-11-26 13:07:55 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -13,3 +13,22 @@ function checkprime(n){
     return true
 }
 console.log(checkprime(72))
+
+
+
+//optimized code
+function checkprime(n){
+    if(n<2){
+        return false
+    }
+    else
+    {
+        for(i=2;i<=Math.sqrt(n);i++){
+            if(n%i ==0){
+                return false
+            }
+        }
+    }
+    return true
+}
+console.log(checkprime(4))
